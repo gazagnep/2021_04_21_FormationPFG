9354a006ec040538cfc9b144148ca837d36dd000
PFG <gazagnep@gmail.com>
2021-04-23 10:33:01 +0200

    Update Bug 2021-04-23_10-32

---
+++ src/App/components/Auth/Auth.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Auth.module.scss';
+
+const Auth = (props) => (
+
+  
+  <div className={styles.Auth} data-testid="Auth">
+    Auth Component
+  </div>
+);
+
+Auth.propTypes = {};
+
+Auth.defaultProps = {};
+
+export default Auth;
